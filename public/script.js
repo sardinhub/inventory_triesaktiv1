6845a8b585c502ed53a8b3edaf707f95c3d4031d
sardinhub <sardindamis.smi@gmail.com>
2026-05-06 09:26:13 +0800

    Fix syntax error pada login handler

--- public/script.js
+++ public/script.js
@@ -545,6 +545,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 errEl.innerText = data.error || data.message || "Login Gagal!";
                 errEl.style.display = 'block';
             }
+        } catch(e) { 
+            errEl.innerText = "Koneksi ke server terputus."; 
+            errEl.style.display = 'block'; 
+        }
     });
     
     // Search Functionality
